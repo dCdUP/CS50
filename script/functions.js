10318eec390b08f708e36406bc35e0e0c9e00742
Niklas <niklasmetzen@tutanota.de>
2022-03-27 00:59:24 +0100

    Julian tips

--- script/functions.js
+++ script/functions.js
@@ -1,5 +1,13 @@
 function rechnen() {
-    let value = document.getElementById("zahl").value;
-    value = parseInt(value) + 1;
-    console.log(value);
+    let eingabe = document.getElementById("zahl").value;
+    value = parseInt(eingabe) + 1;
+    console.log(eingabe);
 }
+
+const update = () => {
+
+}
+
+const button = document.getElementById("Knopf")
+
+button.addEventListener("input", calc)
