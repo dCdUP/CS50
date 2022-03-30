04ead3256018cafe5b3799dc94bbee8ad81f5137
Niklas <niklasmetzen@tutanota.de>
2022-03-30 16:30:27 +0200

    IEC Funktionen erstellt

--- script/functions.js
+++ script/functions.js
@@ -3,22 +3,24 @@ const logging = () => {
     // value = eingabe + 1;
     // console.log(value);
     // let choise = document.getElementById("groeßen").value;
-    // let result = 0
     // console.log(choise);
     // document.getElementById("result").innerHTML = result
-
+    let variableEins = parseInt(document.getElementById("powerTwoInput").value)
+    let variableZwei = parseInt(document.getElementById("powerTenInput").value)
+    console.log(variableEins)
+    console.log(variableZwei)
     console.log(powerTwo())
     console.log(powerTen())
+
+    let result = 0
 
 }
 
 const powerTwo = () =>
 {
     const input = document.getElementById("powerTwoList").value;
-    console.log(input)
     const powerTwoarray = ["kibibyte", "mebibyte", "gibibyte", "tebibyte"];
     let index = powerTwoarray.indexOf(input);
-    console.log(index);
     const powerTwo = [10,20,30,40];
     var powerTwoReturn = powerTwo[index];
     return powerTwoReturn
@@ -27,15 +29,35 @@ const powerTwo = () =>
 const powerTen = () =>
 {
     const input = document.getElementById("powerTenList").value;
-    console.log(input)
     const powerTenarray = ["kilobyte", "megabyte", "gigabyte", "terrabyte"];
     let index = powerTenarray.indexOf(input);
-    console.log(index);
     const powerTen = [3,6,9,12];
     var powerTenReturn = powerTen[index];
-    return powerTenReturn
+    return powerTenReturn;
 }
 
+const IECtoTenFunction = () =>
+{
+    console.log("IECtoTenFunction");
+    let powerTwoInput = parseInt(document.getElementById("powerTwoInput").value);
+    console.log(powerTwoInput)
+    let result = 0;
+    if (powerTwoInput == NaN){
+        result = (powerTwoInput*Math.pow(2,powerTwo()))/Math.pow(10,powerTen())
+        console.log(result, "Ausgefuehrt")
+    }
+    else
+    {
+        alert("Error")
+    };
+}
 
-const button = document.getElementById("knopf")
-button.addEventListener("click", logging)
+const TentoIECFunction = () =>
+{
+    console.log("TentoIECFunction");
+}
+
+const IECToTen = document.getElementById("IECToTen")
+const tenToIEC = document.getElementById("tenToIEC")
+IECToTen.addEventListener("click", IECtoTenFunction)
+tenToIEC.addEventListener("click", TentoIECFunction)
